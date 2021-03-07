646a070c3777b452e170b47bdefa2ee11f73abbc
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2021-03-07 16:33:55 +0000

    Remove parcel-cache directory and ignore.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import React from 'react'
 
-import Header from './components/Header'
-import Main from './components/Main'
-import Card from './components/Card'
-import Footer from './components/Footer'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Card from '../components/Card'
+import Footer from '../components/Footer'
 
 export default function App() {
 	return (
